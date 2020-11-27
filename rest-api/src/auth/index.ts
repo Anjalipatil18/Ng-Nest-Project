32170721f20e754d1f981f023f15138966f3904f
Anjalipatil18 <anjalip18@navgurukul.org>
2020-11-27 19:02:29 +0530

    initial commit

--- rest-api/src/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export { AuthGuard } from './auth.guard';
-export { Authenticate, authenticate } from './auth.middleware';
-export { Roles } from './auth.roles.decorator';
--- rest-api/src/users/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './errors/errors';
-export * from './interfaces/interfaces';
-export * from './models/user.model';
-export * from './schema/user.schema';
-export { UsersController } from './users.controller';
-export { UsersModule } from './users.module';
-export { UsersMapper, UsersRepository } from './users.repository';
-export { UsersService } from './users.service';
