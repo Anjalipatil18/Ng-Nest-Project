d37c60095f4249b71fd8cb8e43cfcd706fd89d9a
Anjalipatil1413 <anjalip@mobifyi.com>
2021-02-15 15:14:50 +0530

    changes in file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './interface/users';
+import { UserService } from './users/users.service';
 
 @Component({
   selector: 'auth-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.getUsers();
   }
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(      
+        res => this.users = res,
+        err => console.log(err)
+      )
+  }
+
+  
 
 }
